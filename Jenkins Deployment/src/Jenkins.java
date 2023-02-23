774dc779e4ac73dea4177b55a95753463b88897f
anweshharsh <anwesh.harsh16@gmail.com>
2023-02-23 16:52:39 +0530

    First Commit

--- Jenkins Deployment/src/Jenkins.java
+++ Jenkins Deployment/src/Jenkins.java
@@ -4,6 +4,7 @@ public class Jenkins {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Fuckers");
+		System.out.println("Hello Morons");
 
 	}
 
